4047372592847196c6438cbead17d4d2a1332376
Juan Pino <dejuata@hotmail.com>
2017-04-11 16:07:21 -0500

    refactor landing

--- apps/tenant/views.py
+++ apps/tenant/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import CreateView, TemplateView
+from .models import Tenant, Domain
 
-# Create your views here.
+
+class TenantCreateView(CreateView):
+    model = Tenant
+    fields = ['nombre_tenant', 'descripcion']
+    success_url = '/registrar-tenant'
+
+    def form_valid(self, form):
+        tenant_registrado = form.instance
+        tenant_registrado.schema_name = tenant_registrado.nombre_tenant
+        self.object = form.save()
+        dominio_tenant = Domain(domain=self.object.nombre_tenant + '.localhost',
+                                is_primary=True,
+                                tenant=tenant_registrado
+                                )
+        dominio_tenant.save()
+        return super(TenantCreateView, self).form_valid(form)
