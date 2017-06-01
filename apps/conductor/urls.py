faf58df8a463e3616f9d17fc46e49e1ca2b80ef7
Juan Pino <dejuata@hotmail.com>
2017-06-01 12:41:55 -0500

    import, export excel conductor

--- apps/conductor/urls.py
+++ apps/conductor/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
 
-from .views import ConductorList, ConductorCreate, ConductorUpdate
+from .views import ConductorList, ConductorCreate, ConductorUpdate, import_data, export_data
 from .ajax import conductor_delete
 
 urlpatterns = [
     url(r'^new$', login_required(ConductorCreate.as_view()), name='conductor_new'),
     url(r'^list/', login_required(ConductorList.as_view()), name='conductor_list'),
     url(r'^edit/(?P<pk>\d+)/$', login_required(ConductorUpdate.as_view()), name='conductor_edit'),
-    url(r'^delete/$', login_required(conductor_delete), name='conductor_delete')
+    url(r'^delete/$', login_required(conductor_delete), name='conductor_delete'),
+    # url(r'^import/', import_data, name='conductor_import'),
+    # url(r'^export/', export_data, name='conductor_export'),
 ]
--- apps/conductor/views.py
+++ apps/conductor/views.py
@@ -3,13 +3,68 @@ from django.core.urlresolvers import reverse_lazy
 from django.contrib.messages.views import SuccessMessageMixin
 
 from .forms import ConductorForm
+from ..cliente.forms import UploadFileForm
 from .models import Conductor
 from .sorting import SortMixin
+
+import django_excel as excel
+
+
+# def import_data(request):
+#     if request.method == 'POST':
+#         form = UploadFileForm(data=request.POST, files=request.FILES)
+#         if form.is_valid():
+#             print('valid form')
+#             request.FILES['file'].save_to_database(
+#                 model=Cliente,
+#                 # initializer=None,
+#                 mapdict={"nit": "nit",
+#                          "razon_social": "razon_social",
+#                          "telefono": 'telefono',
+#                          'correo': 'correo',
+#                          'ciudad': 'ciudad',
+#                          'direccion': 'direccion',
+#                          }
+#             )
+#
+#             return HttpResponseRedirect(reverse_lazy('dashboard:cliente:cliente_list'))
+#         else:
+#             return HttpResponseBadRequest()
+#     else:
+#         form = UploadFileForm()
+#     return render(
+#         request,
+#         'upload_form.html',
+#         {
+#             'form': form,
+#             'title': 'Import excel data into database example',
+#             'header': 'Please upload sample-data.xls:'
+#         })
+#
+#
+# def export_data(request):
+#     # return excel.make_response_from_a_table(Cliente, 'xls', file_name="clientes")
+#     query_sets = Conductor.objects.all()
+#     column_names = ['cedula', 'nombres',
+#                     'apellidos', 'direccion',
+#                     'rh', 'telefono',
+#                     'celular', 'correo',
+#                     'fechaNacimiento', 'nivelEstudio',
+#                     'numLicenciaConduccion', 'docLicenciaConduccion',
+#                     'categoriaLicencia', 'estadoLicencia',
+#                     'organismoTransito'
+#                     'activo_inactivo']
+#     return excel.make_response_from_query_sets(
+#         query_sets,
+#         column_names,
+#         'xls',
+#         file_name="clientes"
+#     )
 
 
 class ConductorList(SortMixin, ListView):
     model = Conductor
-    template_name = 'conductor/conductor_list.html'    
+    template_name = 'conductor/conductor_list.html'
     default_sort_params = ('nombres', 'asc')
 
     def sort_queryset(self, qs, sort_by, order):
