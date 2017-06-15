0720db0fc78e7cb0a5c053f4d7ad4887dd200123
Juan Pino <dejuata@hotmail.com>
2017-06-15 18:10:04 -0500

    configurando archivo settings

--- scr/settings/base.py
+++ scr/settings/base.py
@@ -2,8 +2,10 @@ import os
 from django.core.urlresolvers import reverse_lazy
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+from unipath import Path
 
+BASE_DIR = Path(__file__).ancestor(3)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
