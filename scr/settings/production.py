8945e19763df567595986df98ee5b47b434faa83
AndressCona <andresfav@gmail.com>
2017-06-20 01:00:33 -0500

    scr

--- scr/settings/production.py
+++ scr/settings/production.py
@@ -24,9 +24,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
-
 STATIC_ROOT = os.path.join(BASE_DIR, '..', 'static_collected')
-
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
