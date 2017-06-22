df61309b58bc0c4db62f3a3f65a2a071c41df46e
Juan Pino <juan.david.pino@correounivalle.edu.co>
2017-06-21 22:21:22 -0500

    Update 00_insertar_superusuario.sql

--- querys/00_insertar_superusuario.sql
+++ querys/00_insertar_superusuario.sql
@@ -2,4 +2,4 @@
             id, password, last_login, is_superuser, first_name,
             last_name, email, is_staff, is_active, date_joined, theme)
     VALUES ('1', 'pbkdf2_sha256$30000$JKtHlgC7pG1m$mKXVnUL5POfqaxkdFFDOTbNkbRyTZzAoQZEMO0n5TLA=', '2017-04-14', True, 'admin',
-            'admin', 'admin@scr.com', True, True, '2017-04-14', 1);
+            'admin', 'admin@scr.com', True, True, '2017-04-14', '1');
