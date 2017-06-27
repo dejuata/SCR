a3c512d1d2eade0925106f2aca5e8a743e597265
Juan Pino <dejuata@hotmail.com>
2017-06-27 16:54:41 -0500

    update querys

--- querys/00_insertar_superusuario.sql
+++ querys/00_insertar_superusuario.sql
@@ -1,5 +1,5 @@
 ﻿INSERT INTO public.users_mycustomemailuser(
             id, password, last_login, is_superuser, first_name,
             last_name, email, is_staff, is_active, date_joined, theme)
-    VALUES ('1', 'pbkdf2_sha256$30000$JKtHlgC7pG1m$mKXVnUL5POfqaxkdFFDOTbNkbRyTZzAoQZEMO0n5TLA=', '2017-04-14', True, 'admin',
+    VALUES ('0', 'pbkdf2_sha256$30000$JKtHlgC7pG1m$mKXVnUL5POfqaxkdFFDOTbNkbRyTZzAoQZEMO0n5TLA=', '2017-04-14', True, 'admin',
             'admin', 'admin@scr.com', True, True, '2017-04-14', '1');
--- querys/02_relacionar_dominio.sql
+++ querys/02_relacionar_dominio.sql
@@ -1,3 +1,3 @@
 ﻿INSERT INTO public.tenant_domain(
             id, domain, is_primary, tenant_id)
-    VALUES (1, 'localhost', True, 2);
+    VALUES (0, 'localhost', True, 0);
